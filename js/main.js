1eef7944180307372166e9041dcaee6956852383
Sivachel <lsivachelvan@spartaglobal.com>
2018-08-15 11:30:50 +0100

    Sorted indentation in CSS and added variables in JS

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
 
+// DOM objects into variables
+var container = $('.mini');
+var highscore = $('.highscore');
+var yourscore = $('.yourscore');
+var line1 = $('#line1');
+var line2 = $('#line2');
+var line3 = $('#line3');
+var line4 = $('#line4');
+var car = $('#car');
+var car1 = $('#car_1');
+var car2 = $('#car_2');
+var car3 = $('#car_3');
+var car4 = $('#car_4');
+var Player2turn = $('.Player2turn');
+var restart = $('.restart');
+
 
 
 
